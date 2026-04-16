2fedc2622eaebfc104cfed6e26a3f32009777eb4
Mike J. Renaker / "MikeDEV <mikejrenaker@proton.me>
2026-04-15 23:05:01 -0400

    misc: expand options present for STUN/TURN

--- index.js
+++ index.js
@@ -90,21 +90,35 @@
       iceTransportPolicy: 'all',
       iceServers: [
         {
-          urls: 'stun:vpn.mikedev101.cc:3478'
+          urls: [
+            'stun:vpn.mikedev101.cc:3478',
+            'stun:vpn.mikedev101.cc:5349'
+          ]
         },
         {
-          urls: 'turn:vpn.mikedev101.cc:3478',
+          urls: [
+            'turn:vpn.mikedev101.cc:3478',
+            'turn:vpn.mikedev101.cc:5349'
+          ],
           username: 'free',
           credential: 'free'
         },
         {
-          urls: 'stun:vpn.mikedev101.cc:5349'
+          urls: [
+            'stun:stun.l.google.com:19302',
+            'stun:stun.l.google.com:5349',
+            'stun:stun1.l.google.com:3478',
+            'stun:stun1.l.google.com:5349',
+            'stun:stun2.l.google.com:19302',
+            'stun:stun2.l.google.com:5349',
+            'stun:stun3.l.google.com:3478',
+            'stun:stun3.l.google.com:5349',
+            'stun:stun4.l.google.com:19302',
+            'stun:stun4.l.google.com:5349',
+            'stun.cloudflare.com:3478',
+            'stun.cloudflare.com:53'
+          ]
         },
-        {
-          urls: 'turn:vpn.mikedev101.cc:5349',
-          username: 'free',
-          credential: 'free'
-        }
       ]
     }
   }
